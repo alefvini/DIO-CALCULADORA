03a7b20671d738ea3aaceccc138278e90b21b282
Alef Vinícius <116196987+alefvini@users.noreply.github.com>
2022-12-14 23:58:27 -0300

    Estilização final

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ const App = () => {
     if(firstNumber === '0'){
         setFirstNumber(String(currentNumber));
         setCurrentNumber('0')
-        setOperation('-')
+        setOperation('/')
     }else {
       const sum = Number(firstNumber) / Number(currentNumber);
       setCurrentNumber(String(sum))
@@ -67,7 +67,7 @@ const App = () => {
     if(firstNumber === '0'){
         setFirstNumber(String(currentNumber));
         setCurrentNumber('0')
-        setOperation('-')
+        setOperation('x')
     }else {
       const sum = Number(firstNumber) * Number(currentNumber);
       setCurrentNumber(String(sum))
@@ -84,9 +84,15 @@ const App = () => {
             handleSumNumbers();
             break;
           case '-':
-            handleMinusNumbers();
+            handleMinusNumbers(); 
             break;
-          default: 
+          case '/':
+            handleDivideNumbers();
+            break;
+          case 'x':
+            handleMultiplNumbers();
+            break;
+          default:
             break;
         }
     }
@@ -98,28 +104,33 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x" onClick={handleMultiplNumbers}/>
-          <Button label="/" onClick={handleDivideNumbers}/>
           <Button label="c" onClick={handleOnClear}/>
-          <Button label="." onClick={() => handleAddNumber('.')}/>
+          <Button label="=" onClick={handleEquals}/>
+        </Row>
+        <Row>
+          <Button label="x" onClick={handleMultiplNumbers}/>
+          <Button label="-" onClick={handleMinusNumbers}/>
+          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="/" onClick={handleDivideNumbers}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={handleEquals}/>
+        </Row>
+        <Row>
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="." onClick={() => handleAddNumber('.')}/>
         </Row>
       </Content>
     </Container>
--- src/styles.js
+++ src/styles.js
@@ -3,13 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
-    background-color: #FAFAFA;
-
+    background: rgb(192,192,192);
+    background: radial-gradient(circle, rgba(192,192,192,1) 0%, rgba(116,106,106,1) 100%);
     display: flex;
     align-items: center;
     justify-content: center;
 `
-
 
 export const Content = styled.div`
     background-color: #FFFFFF;
